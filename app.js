3ddf57910c5ad6e7f563562a61726ff5ab0381ad
beneti <beneti@beneti.com.br>
2013-02-10 17:29:41 -0200

    removed user references

--- app.js
+++ app.js
@@ -5,9 +5,9 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , shortener = require('./routes/shortener');
 
 var app = express();
 
@@ -28,7 +28,8 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.post('/generate', shortener.generate);
+app.get('/:short', shortener.redirect);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
