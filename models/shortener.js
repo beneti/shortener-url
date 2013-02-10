67c834bfc5b523b24b1f574a4e1ae6ceb0c23661
beneti <beneti@beneti.com.br>
2013-02-10 17:31:03 -0200

    added shortener model

---
+++ models/shortener.js
@@ -0,0 +1,54 @@
+var mongoose = require('mongoose');
+var Url = require('url');
+mongoose.connect('mongodb://localhost/shortener');
+var schema = mongoose.Schema({ id: 'number', url: 'string' });
+var Shortener = mongoose.model('Shortener', schema);
+var alphabet, base;
+
+alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".split("");
+
+base = alphabet.length;
+
+function encode(i) {
+  var s;
+  if (i === 0) {
+    return alphabet[0];
+  }
+  s = "";
+  while (i > 0) {
+    s += alphabet[i % base];
+    i = parseInt(i / base, 10);
+  }
+  return s.split("").reverse().join("");
+};
+
+function decode(s) {
+  var c, i, _i, _len;
+  i = 0;
+  for (_i = 0, _len = s.length; _i < _len; _i++) {
+    c = s[_i];
+    i = i * base + alphabet.indexOf(c);
+  }
+  return i;
+};
+
+exports.generate = function(url, shortener){
+  var parsed = Url.parse(url);
+  var correct_url = parsed.procol === undefined ? 'http://' + url: url;
+
+  var id = parseInt(new Date().getTime()/1000);
+  var short = encode(id);
+
+  var newurl = new Shortener({ id: id, url: correct_url });
+  newurl.save(function(err){
+    shortener(short.toString());
+  });
+};
+
+exports.decode = function(short, url){
+  var id = decode(short);
+  Shortener.findOne({id: id}, function(err, shortener){
+    url(shortener.url);
+  });
+};
+
